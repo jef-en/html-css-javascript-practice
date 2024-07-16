8189786de7de52a0e1d2c41ac7f4adb452f39f73
Jefen De Villa <jedevilla@gbox.adnu.edu.ph>
2024-07-16 17:55:18 +0800

    minor adjustment

---
+++ Javascript/Pokedex/script.js
@@ -0,0 +1,4 @@
+const pokemonAPI = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/";
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const pokemonName = document.getElementById("pokemon-name");
